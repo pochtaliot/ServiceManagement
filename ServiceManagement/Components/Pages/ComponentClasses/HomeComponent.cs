1b1d633dd72408c3ddac8d2e8e73eaf7371523bf
Denis Sergeev <pochtaliot@gmail.com>
2025-04-07 19:04:36 +0200

    parallel attempt 2

--- ServiceManagement/Components/Pages/ComponentClasses/HomeComponent.cs
+++ ServiceManagement/Components/Pages/ComponentClasses/HomeComponent.cs
@@ -6,4 +6,16 @@ namespace ServiceManagement.Components.Pages.ComponentClasses;
 public class HomeComponent : ComponentBase
 {
     [Inject] protected PreloadService PreloadService { get; set; } = null!;
+
+    protected bool componentsLoaded = false;
+
+    protected override async Task OnInitializedAsync()
+    {
+        // Start loading both components in parallel
+        var serviceTask = Task.Run(() => componentsLoaded = true);
+        var appPoolTask = Task.Run(() => componentsLoaded = true);
+
+        await Task.WhenAll(serviceTask, appPoolTask);
+        StateHasChanged();
+    }
 }
